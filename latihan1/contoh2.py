a80fb961232d2905c9591430906ee3021fb9448e
Ferli <125356066+MasFer27@users.noreply.github.com>
2023-03-23 10:28:39 +0700

    Delete latihan1 directory

--- latihan1/contoh2.py
+++
@@ -1,9 +0,0 @@
-class Mahasiswa:
-    def __init__(self, nama, nim):
-        self.nama = nama
-        self.nim = nim
-    def info(self):
-        print(f"Nama: {self.nama}\nNIM: {self.nim}")
-
-mahasiswaB = Mahasiswa("Ferli", "210511140")
-mahasiswaB.info()
