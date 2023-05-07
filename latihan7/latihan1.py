be6d246df657f4032d8352745256eb0414c5e1ad
Ferli <125356066+MasFer27@users.noreply.github.com>
2023-05-07 19:31:02 +0700

    Add files via upload

---
+++ latihan7/latihan1.py
@@ -0,0 +1,16 @@
+#NAMA : FERLI
+#NIM     : 210511140
+#KELAS: TI21 D
+
+class Person:
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+person = Person("John", 30)
+# Menambah atribut address secara dinamis
+person.address = "123 Main St."
+# Mengubah nilai atribut age secara dinamis
+person.age = 35
+print(person.name)
+print(person.age)
+print(person.address)
